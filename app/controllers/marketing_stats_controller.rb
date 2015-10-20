495d7753013a09bd780d1c897bc1690cfbe945c3
Hugo Melendez <hemelend@gmail.com>
2015-10-19 22:28:23 -0600

    added more style

--- app/controllers/marketing_stats_controller.rb
+++ app/controllers/marketing_stats_controller.rb
@@ -4,7 +4,7 @@ class MarketingStatsController < ApplicationController
   end
 
   def show
-    ms = ImportedFile.find(params[:id]).marketing_stats
-    @mstats = Kaminari.paginate_array(ms).page(params[:page] || 1).per(100)
+    @impfile = ImportedFile.find(params[:id])
+    @mstats = Kaminari.paginate_array(@impfile.marketing_stats).page(params[:page] || 1).per(100)
   end
 end
